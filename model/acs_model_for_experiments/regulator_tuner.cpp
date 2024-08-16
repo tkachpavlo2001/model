396f8f211191bb7a0334ac09e1347cc0beb75e46
Pavlo Tkach <tkachpavlo2001@gmail.com>
2024-08-16 16:05:27 +0300

    Changes: regulator_tuner & CMakeList.txt Added: functions my_function, my_function_gradiend, 	my_function_f_and_gradient Problems discovered: CMakeList.txt do not include the GSL library
    Next objective: to resolve the problem with the GSL library
    		AND THE SAME
    		(to develop regulator_tuner and fulfill it)

--- model/acs_model_for_experiments/regulator_tuner.cpp
+++ model/acs_model_for_experiments/regulator_tuner.cpp
@@ -26,11 +26,60 @@ long double fitness_function_varied_reference_signal
 
     return std::accumulate(records.begin(), records.end(), 0, [&](double acc, double num)->double {return acc + num * num;} );
 }
+long double fitness_function_varied_reference_signal(user_parameters_for_gsl_optimizer* p_my_parameters)
+{
+    return fitness_function_varied_reference_signal(
+        p_my_parameters->p_tuner,
+        p_my_parameters->dt,
+        p_my_parameters->length,
+        p_my_parameters->t_registrate,
+        p_my_parameters->times,
+        p_my_parameters->min,
+        p_my_parameters->max
+        );
+}
+double my_function(const gsl_vector * _p_vector, void * _p_user_parameters)
+{
+    user_parameters_for_gsl_optimizer * p_my_parameters = (user_parameters_for_gsl_optimizer *) _p_user_parameters;
+    p_my_parameters->p_regulator->to_set_koefficients(
+                gsl_vector_get(_p_vector,0),
+                gsl_vector_get(_p_vector,1),
+                gsl_vector_get(_p_vector,2)
+                );
+    return fitness_function_varied_reference_signal(p_my_parameters);
+}
+void my_function_gradient(const gsl_vector * _p_vector, void * _p_user_parameters, gsl_vector * _p_gradient_vector)
+{
+    user_parameters_for_gsl_optimizer * p_my_parameters = (user_parameters_for_gsl_optimizer *) _p_user_parameters;
+    double previous_value;
 
-Regulator_tuner_iterface::Regulator_tuner_iterface(){}
+    previous_value = p_my_parameters->p_regulator->to_check_parameters()[PID_regulator::K_P];
+    p_my_parameters->p_regulator->to_get_parameters()[PID_regulator::K_P] += p_my_parameters->h;
+    gsl_vector_set(_p_gradient_vector, 0, fitness_function_varied_reference_signal(p_my_parameters) - p_my_parameters->last_value_f);
+    p_my_parameters->p_regulator->to_get_parameters()[PID_regulator::K_P] = previous_value;
 
+    previous_value = p_my_parameters->p_regulator->to_check_parameters()[PID_regulator::K_I];
+    p_my_parameters->p_regulator->to_get_parameters()[PID_regulator::K_I] += p_my_parameters->h;
+    gsl_vector_set(_p_gradient_vector, 0, fitness_function_varied_reference_signal(p_my_parameters) - p_my_parameters->last_value_f);
+    p_my_parameters->p_regulator->to_get_parameters()[PID_regulator::K_I] = previous_value;
+
+    previous_value = p_my_parameters->p_regulator->to_check_parameters()[PID_regulator::K_D];
+    p_my_parameters->p_regulator->to_get_parameters()[PID_regulator::K_D] += p_my_parameters->h;
+    gsl_vector_set(_p_gradient_vector, 0, fitness_function_varied_reference_signal(p_my_parameters) - p_my_parameters->last_value_f);
+    p_my_parameters->p_regulator->to_get_parameters()[PID_regulator::K_D] = previous_value;
+}
+void my_functions_f_and_gradient(const gsl_vector * _p_vector, void * _p_user_parameters, double * f, gsl_vector* _p_gradient_vector)
+{
+    user_parameters_for_gsl_optimizer * p_my_parameters = (user_parameters_for_gsl_optimizer *) _p_user_parameters;
+    *f = p_my_parameters->last_value_f = my_function(_p_vector, _p_user_parameters);
+    my_function_gradient(_p_vector, _p_user_parameters, _p_gradient_vector);
+}
+
+Regulator_tuner_iterface::Regulator_tuner_iterface(Automated_control_system * _m, PID_regulator * _r)
+{
+    to_set_model_and_regulator(_m, _r);
+}
 Regulator_tuner_iterface::~Regulator_tuner_iterface(){}
-
 void Regulator_tuner_iterface::to_set_model_and_regulator(Automated_control_system * _m, PID_regulator * _c)
 {
     acs_model = _m;
@@ -48,6 +97,14 @@ PID_regulator * Regulator_tuner_iterface::to_get_regulator()
 {
     return regulator;
 }
+
+Regulator_tuner_gradient_method::Regulator_tuner_gradient_method(Automated_control_system * _m, PID_regulator * _r) : Regulator_tuner_iterface(_m, _r)
+{
+
+}
+Regulator_tuner_gradient_method::~Regulator_tuner_gradient_method()
+{}
+
 Regulator_tuner::Regulator_tuner()
 {
 
--- model/acs_model_for_experiments/regulator_tuner.h
+++ model/acs_model_for_experiments/regulator_tuner.h
@@ -4,23 +4,57 @@
 
 #include"automated_control_system.h"
 
+#include<gsl/gsl_multimin.h>
+
 #include <memory>
 
 class Regulator_tuner_iterface;
 long double fitness_function_varied_reference_signal
 (Regulator_tuner_iterface* _tuner, double _dt, double _length, double _t_registrate, double _times, double _min, double _max);
+struct user_parameters_for_gsl_optimizer
+{
+    Regulator_tuner_iterface * p_tuner;
+    Automated_control_system * p_acs_model;
+    PID_regulator * p_regulator;
+    double dt;
+    double length;
+    double t_registrate;
+    double times;
+    double min;
+    double max;
+    double last_value_f;
+    double h;
+};
+long double fitness_function_varied_reference_signal
+(user_parameters_for_gsl_optimizer*);
+double my_function(const gsl_vector *, void *);
+void my_function_gradient(const gsl_vector *, void *, gsl_vector *);
+void my_functions_f_and_gradient(const gsl_vector*, void*, double*, gsl_vector*);
+
 class Regulator_tuner_iterface
 {
 private:
     Automated_control_system * acs_model;
     PID_regulator * regulator;
 public:
-    Regulator_tuner_iterface();
+    Regulator_tuner_iterface(Automated_control_system * = nullptr, PID_regulator * = nullptr);
     virtual ~Regulator_tuner_iterface() = 0;
     void to_set_model_and_regulator(std::shared_ptr<Automated_control_system>, std::shared_ptr<PID_regulator>);
     void to_set_model_and_regulator(Automated_control_system *, PID_regulator *);
     Automated_control_system * to_get_model();
     PID_regulator * to_get_regulator();
+};
+
+class Regulator_tuner_gradient_method : virtual public Regulator_tuner_iterface
+{
+private:
+    const gsl_multimin_fdfminimizer_type * T;
+    gsl_multimin_fdfminimizer * s = nullptr;
+    gsl_vector * x;
+    gsl_multimin_function_fdf my_function;
+public:
+    Regulator_tuner_gradient_method(Automated_control_system * = nullptr, PID_regulator * = nullptr);
+    virtual ~Regulator_tuner_gradient_method();
 };
 
 class Regulator_tuner : public Regulator_tuner_iterface
