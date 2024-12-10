f90b4286a925a237aa547ee782aeb955be70035b
Pavlo Tkach <tkachpavlo2001@gmail.com>
2024-12-10 15:23:33 +0200

    model initialisation is doing through the common start function the common part

--- model/acs_model_for_experiments/chartwidgetfactory.cpp
+++ model/acs_model_for_experiments/chartwidgetfactory.cpp
@@ -69,7 +69,16 @@ void iChartWidget::_to_model_init()
     _source->to_receive_input_signal(20);
 */
 
-    ;
+
+    //_pRegistrator = Registrator_qt::to_new(this);
+    _pRegistrator->to_set_series(_pSeries);
+
+    _source = std::make_shared<DC_source_inerted>();
+
+    _acs_model = std::make_shared<Automated_control_system_paralleled>();
+
+    _experiment = std::make_shared<Experiment_executor>();
+    _experiment->to_get_model_to_run(_acs_model.get());
 
 }
 
@@ -77,15 +86,10 @@ void ChartWidget_velocity::_to_model_init()
 {
     iChartWidget::_to_model_init();
 
-    //_pRegistrator = Registrator_qt::to_new(this);
-    _pRegistrator->to_set_series(_pSeries);
-
     const Default_configuration_setter default_configuration_setter_obj;
-    // reference case
-    ///_definder = std::make_shared<Reference_signal_definder_static>();
-    ///_regulator = std::make_shared<PID_regulator>();
-    _source = std::make_shared<DC_source_inerted>();
+
     _process = std::make_shared<DC_engine_velocity_out>();
+    _process->to_set_calculation_mode(DC_engine::EULER);
 
     default_configuration_setter_obj.to_set_elements_parameters(
                 _definder,
@@ -94,17 +98,12 @@ void ChartWidget_velocity::_to_model_init()
                 _process
                 );
 
-    _process->to_set_calculation_mode(DC_engine::EULER);
-
-    _acs_model = std::make_shared<Automated_control_system_paralleled>();
     _acs_model->to_mount_the_element(_definder.get());
     _acs_model->to_mount_the_element(_regulator.get());
     _acs_model->to_mount_the_element(_source.get());
     _acs_model->to_mount_the_element(_process.get());
 
 
-    _experiment = std::make_shared<Experiment_executor>();
-    _experiment->to_get_model_to_run(_acs_model.get());
     default_configuration_setter_obj.to_set_experiment_parameters(_experiment);
     //set
 
@@ -149,15 +148,10 @@ void ChartWidget_theta::_to_model_init()
 {
     iChartWidget::_to_model_init();
 
-    //_pRegistrator = Registrator_qt::to_new(this);
-    _pRegistrator->to_set_series(_pSeries);
-
     const Default_configuration_setter default_configuration_setter_obj;
-    // reference case
-    ///_definder = std::make_shared<Reference_signal_definder_static>();
-    ///_regulator = std::make_shared<PID_regulator>();
-    _source = std::make_shared<DC_source_inerted>();
+
     _process = std::make_shared<DC_engine>();
+    _process->to_set_calculation_mode(DC_engine::EULER);
 
     default_configuration_setter_obj.to_set_elements_parameters(
                 _definder,
@@ -166,17 +160,11 @@ void ChartWidget_theta::_to_model_init()
                 _process
                 );
 
-    _process->to_set_calculation_mode(DC_engine::EULER);
-
-    _acs_model = std::make_shared<Automated_control_system_paralleled>();
     _acs_model->to_mount_the_element(_definder.get());
     _acs_model->to_mount_the_element(_regulator.get());
     _acs_model->to_mount_the_element(_source.get());
     _acs_model->to_mount_the_element(_process.get());
 
-
-    _experiment = std::make_shared<Experiment_executor>();
-    _experiment->to_get_model_to_run(_acs_model.get());
     default_configuration_setter_obj.to_set_experiment_parameters(_experiment);
     //set
 
@@ -221,15 +209,14 @@ void ChartWidget_regulator::_to_model_init()
 {
     iChartWidget::_to_model_init();
 
-    //_pRegistrator = Registrator_qt::to_new(this);
-    _pRegistrator->to_set_series(_pSeries);
-
     const Default_configuration_setter default_configuration_setter_obj;
+
     // reference case
     _definder = std::make_shared<Reference_signal_definder_static>();
     _regulator = std::make_shared<PID_regulator>();
-    _source = std::make_shared<DC_source_inerted>();
     _process = std::make_shared<DC_engine>();
+
+    _process->to_set_calculation_mode(DC_engine::EULER);
 
     default_configuration_setter_obj.to_set_elements_parameters(
                 _definder,
@@ -238,17 +225,11 @@ void ChartWidget_regulator::_to_model_init()
                 _process
                 );
 
-    _process->to_set_calculation_mode(DC_engine::EULER);
-
-    _acs_model = std::make_shared<Automated_control_system_paralleled>();
     _acs_model->to_mount_the_element(_definder.get());
     _acs_model->to_mount_the_element(_regulator.get());
     _acs_model->to_mount_the_element(_source.get());
     _acs_model->to_mount_the_element(_process.get());
 
-
-    _experiment = std::make_shared<Experiment_executor>();
-    _experiment->to_get_model_to_run(_acs_model.get());
     default_configuration_setter_obj.to_set_experiment_parameters(_experiment);
     //set
 
